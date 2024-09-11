e554c2b97591b258868447940734607119a1c7f1
Alisa Sireneva <me@purplesyringa.moe>
2024-09-11 09:18:52 +0300

    Update license information

--- include/clipper.hpp
+++ include/clipper.hpp
@@ -1,6 +1,6 @@
 /*
     Clipper: C++ CLI library
-    Copyright (C) 2022  Ivanq
+    Copyright (C) 2022  Alisa Sireneva
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
